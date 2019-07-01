6ad52c2130ac481a67032f1e850a3c04d1d3e98c
Kirill Sidorov <kirillsidorov@gmail.com>
2019-07-01 18:36:06 +0300

    Wiki common words and save to file

--- DataGathering/lesson2/wiki.py
+++ DataGathering/lesson2/wiki.py
@@ -1,5 +1,6 @@
 from requests import get
 import re
+from collections import Counter
 
 
 def get_link(topic):
@@ -13,7 +14,7 @@ def get_topic_page(topic):
     return html
 
 
-def get_topic_text(topic):
+def get_topic_russian_words(topic):
     html_content = get_topic_page(topic)
     words = re.findall("[а-яА-ЯёЁ]{3,}", html_content)
     return words
@@ -28,7 +29,20 @@ def get_wiki_links(topic):
     return topics
 
 
-def get_common_words
+def get_common_words(words: list, top=10):
+    return Counter(words).most_common(top)
+
+
+def save_common_words_to_file(words: list, topic_name):
+    with open(topic_name+'_cw.txt', 'w') as f_output:
+        f_output.write(f'{len(words)} часто встречающихся слов на странице '
+                       f'https://ru.wikipedia.org/wiki/{topic_name.capitalize()}'
+                       + '\n\n')
+        for word in words:
+            f_output.write(f'Слово "{word[0]}" встрачается {word[1]} раз' + '\n')
+
 
 if __name__ == "__main__":
-    print(get_wiki_links('россия'))
+    topic_ = 'россия'
+    save_common_words_to_file(get_common_words(get_topic_russian_words(topic_,), 10), topic_)
+
