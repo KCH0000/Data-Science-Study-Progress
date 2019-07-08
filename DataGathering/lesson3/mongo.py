1cb0d62bbb47f4534e960a970808bd370cd42dc8
Kirill Sidorov <kirillsidorov@gmail.com>
2019-07-08 18:50:22 +0300

    Mongo save uniq

--- DataGathering/lesson3/mongo.py
+++ DataGathering/lesson3/mongo.py
@@ -1,11 +1,11 @@
-from pymongo import MongoClient, errors as mongo_e  # клиент к mongodb
+from pymongo import MongoClient, ASCENDING, errors as mongo_e  # клиент к mongodb
 from urllib.parse import quote_plus  # Форматирование пользователя и пароля
 
 
 DB_URL = 'localhost'
 DB_USERNAME = 'root'
 DB_PASSWORD = 'root'
-SAVE_TO_DB = False  # True - программа будет читать фаил и сохранять в базу. False - будут работать тольк чтение
+SAVE_TO_DB = True  # True - программа будет читать фаил и сохранять в базу. False - будут работать тольк чтение
 VAL = {'USD': 63, 'EUR': 72, 'руб.': 1}  # Курсы
 
 
@@ -48,37 +48,42 @@ def load_hh_data_from_file(file):
 
 
 def save_hh_to_mongodb(db, coll_name, vacancies):
+    success, errors = 0, 0
     try:
         coll = db[coll_name]
     except mongo_e.CollectionInvalid:
         print(f'Mongo: {coll_name} - коллекция недоступна')
         return False
-    try:
-        ids = coll.insert_many(vacancies)
-        print(f'Mongo: добавлено {len(ids.inserted_ids)} записей в коллецию {coll_name}')
-        return ids.inserted_ids
-    except mongo_e.WriteError:
-        print('Mongo: ошибка записи')
-        return False
-    except Exception as e:
-        print(f'Mongo: ошибка {e}')
+    coll.create_index([('url', ASCENDING)], unique=True)
+    for vacancy in vacancies:
+        try:
+            coll.insert_one(vacancy)
+            success += 1
+        except mongo_e.DuplicateKeyError:
+            # print(f'Mongo: запись уже существуе \n {vacancy} ')
+            errors += 1
+        except Exception as e:
+            print(f'Mongo: ошибка {e}')
+    print(f'Mongo: добавлено {success}, ошибок {errors} в коллекции {coll_name}')
+    return True
 
 
-def get_vacancies_more(db, coll_name, compensation):
+def get_vacancies_more(db, coll_name, compensation, max_empty=True):
     vacancies = []
     try:
         coll = db[coll_name]
     except mongo_e.CollectionInvalid:
         print(f'Mongo: {coll_name} - коллекция недоступна')
-        return False
+        return []
     for currency, curs in VAL.items():
-        for vacancy in coll.find({'$and': [
-                                    {'currency': currency},
-                                    {'$or': [
-                                        {'max': 0},
-                                        {'max': {'$gt': compensation/curs}}
-                                    ]}
-        ]}):
+        query = {'$and': [
+                    {'currency': currency},
+                    {'$or': [
+                        {'max': 0 if max_empty else -1},
+                        {'max': {'$gt': compensation/curs}}
+                        ]}
+        ]}
+        for vacancy in coll.find(query):
             vacancies.append(vacancy)
     return vacancies
 
@@ -89,5 +94,5 @@ if __name__ == '__main__':
         vacancies_ = load_hh_data_from_file('output.txt')
         if vacancies_:
             save_hh_to_mongodb(db_, 'vacancies', vacancies_)
-    for x in get_vacancies_more(db_, 'vacancies', 300000):
+    for x in get_vacancies_more(db_, 'vacancies', 250000, max_empty=False):
         print(x)
